d6fdf3c6bd81204437b4af372be25aa658f77935
Jonathan <jonnyarchy@gmail.com>
2023-07-08 14:34:40 +0100

    More UI things

---
+++ Assets/Scenes/JonnyScene/GameUIManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameUIManager : VisualElement
+{
+    VisualElement m_LeaveArea;
+
+    public new class UxmlFactory : UxmlFactory<GameUIManager, UxmlTraits> { }
+
+    public GameUIManager()
+    {
+        this.RegisterCallback<GeometryChangedEvent>(OnGeometryChange);
+    }
+
+    void OnGeometryChange(GeometryChangedEvent evt)
+    {
+        m_LeaveArea = this.Q("btnQuit");
+
+        m_LeaveArea?.RegisterCallback<ClickEvent>(ev => ClickedButton());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void ClickedButton()
+    {
+
+        Debug.Log("Clicked quit game");
+    }
+}
