b455cebbeee17b28ce781f0f661f5b0d81b5cca9
Jonathan <jonnyarchy@gmail.com>
2023-07-08 21:18:24 +0100

    Title Screen sads

---
+++ Assets/Scenes/JonnyScene/WaitForAudio.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForAudio : MonoBehaviour {
+    GameObject AudioHauntedHouse;
+    AudioSource AudioSourceAudioHauntedHouse;
+    AudioClip AudioClipHauntedHouse;
+
+    TitleUIManager TitleUIManager;
+
+    void Start() {
+        AudioHauntedHouse = GameObject.Find("AudioHauntedHouse");
+        AudioSourceAudioHauntedHouse = AudioHauntedHouse.GetComponent<AudioSource>();
+        AudioClipHauntedHouse = AudioSourceAudioHauntedHouse.clip;
+    }
+
+    void Update() {
+      //  if (GetComponent<TitleUIManager>("clickedStart")) {
+        //    StartCoroutine("WaitForAudioEnd", AudioClipHauntedHouse.length);
+        //}
+    }
+
+    void WaitForAudioEnd() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+}
