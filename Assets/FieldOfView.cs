d5a22fc000947b6340df5ceb0315ad37f3e28c94
Piotrus Watson <piotruswatson@gmail.com>
2023-07-08 17:46:56 +0100

    managed to do some stuff i guess???

--- Assets/FieldOfView.cs
+++ Assets/FieldOfView.cs
@@ -10,6 +10,30 @@ public class FieldOfView : MonoBehaviour
     
     public LayerMask targetMask;
     public LayerMask obstacleMask;
+
+
+    IEnumerator FindTargetsWithDelay(float delay){
+        while(true){
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+    public List<Transform> visibleTargets;
+    void FindVisibleTargets(){
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i=0; i < targetsInViewRadius.Length; i++){
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2){
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask)){
+                    visibleTargets.Add(target);
+                }
+            }
+
+        }
+    }
     public Vector3 DirectionFromAngle(float angle, bool angleIsGlobal){
         if (!angleIsGlobal) {
             angle += transform.eulerAngles.y;
