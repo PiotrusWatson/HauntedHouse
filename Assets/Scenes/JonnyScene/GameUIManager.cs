a2f817f770c9184339520dd0ef3f8ba6191821e8
Jonathan <jonnyarchy@gmail.com>
2023-07-08 16:29:37 +0100

    It's funny but I need the name of the game for this

--- Assets/Scenes/JonnyScene/GameUIManager.cs
+++ Assets/Scenes/JonnyScene/GameUIManager.cs
@@ -1,29 +1,48 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class GameUIManager : VisualElement
-{
+public class GameUIManager : VisualElement {
     VisualElement btnQuit;
     VisualElement btnStart;
+
+    bool clickedQuit = false;
+    bool clickedStart = false;
+
+    GameObject AudioHauntedHouse;
+    AudioSource AudioSourceAudioHauntedHouse;
+    AudioClip AudioClipHauntedHouse;
+
     public new class UxmlFactory : UxmlFactory<GameUIManager, UxmlTraits> { }
-    public GameUIManager()
-    {
+    public GameUIManager() {
         RegisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
-    void OnGeometryChange(GeometryChangedEvent evt)
-    {
+    void OnGeometryChange(GeometryChangedEvent evt) {
         btnQuit = this.Q("btnQuit");
         btnQuit?.RegisterCallback<ClickEvent>(ev => ClickedQuit());
 
         btnStart = this.Q("btnStart");
         btnStart?.RegisterCallback<ClickEvent>(ev => ClickedStart());
     }
-    void ClickedQuit()
-    {
-        Debug.Log("Clicked quit game");
+    void Start() {
     }
-    void ClickedStart()
-    {
-        Debug.Log("Clicked start game");
+    void Update() {
+    }
+    void ClickedQuit() {
+        Debug.Log("Clicked quit game");
+        clickedQuit = true;
+    }
+    void ClickedStart() {
+        AudioHauntedHouse = GameObject.Find("AudioHauntedHouse");
+        AudioSourceAudioHauntedHouse = AudioHauntedHouse.GetComponent<AudioSource>();
+        AudioClipHauntedHouse = AudioSourceAudioHauntedHouse.clip;
+
+        Debug.Log("game start");
+        clickedStart = true;
+
+        PlayHauntedHouse();
+        clickedStart = false;
+    }
+    void PlayHauntedHouse() {
+        AudioSourceAudioHauntedHouse.Play();
     }
 }
