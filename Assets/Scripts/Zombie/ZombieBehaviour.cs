0539ef4603d857e55466a65d725b0a1714878a11
Piotrus Watson <piotruswatson@gmail.com>
2023-08-14 21:03:52 -0400

    made everything only pull from health

--- Assets/Scripts/Zombie/ZombieBehaviour.cs
+++ Assets/Scripts/Zombie/ZombieBehaviour.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(NavigationManager))]
+[RequireComponent(typeof(DamageInRadius))]
 public class ZombieBehaviour : MonoBehaviour
 {
     public GameObject target;
     NavigationManager navigationManager;
+    DamageInRadius damager;
 
     // Start is called before the first frame update
     void Awake()
     {
         navigationManager = GetComponent<NavigationManager>();
+        damager = GetComponent<DamageInRadius>();
     }
 
     // Update is called once per frame
     void Update()
     {
         navigationManager.SetDestination(target.transform.position);
+        damager.Damage();
+
     }
 }
